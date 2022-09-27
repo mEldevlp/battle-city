9d3cfbd64765a8b030274444c9761898a69dc265
Daniel <indydevlp@vk.com>
2022-09-27 20:54:37 +0300

    testing Glad lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-
+#include <iostream>
 int main(void)
 {
     GLFWwindow* window;
@@ -18,12 +19,22 @@ int main(void)
 
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
+	
+	if(!gladLoadGL())
+	{
+		std::cout << "Cant load GLAD" << std::endl;
+		return -1;
+	}
+	
+	std::cout << "OpenGL " << GLVersion.major << "." << GLVersion.minor << std::endl;
+	
+	glClearColor(0, 1, 0, 1);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        //glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
